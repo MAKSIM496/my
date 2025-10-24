008ae0fdbab9a9baeb56b0a622cf5b805c47961f
MAKSIM496 <spmm8537717@mail.ru>
2025-10-24 19:51:16 +0300

    Изменения

--- js.js
+++
@@ -1,8 +0,0 @@
-// Находим кнопку
-const actionBtn = document.getElementById('actionBtn');
-
-// Обработчик клика
-actionBtn.addEventListener('click', () => {
-    alert('Вы нажали на кнопку!');
-    actionBtn.innerHTML = '<i class="fas fa-check"></i> Спасибо!';
-});
